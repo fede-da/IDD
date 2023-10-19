85753e3ae3f6dacda734f85b3ccf0cabf73deb77
kaguyasama <fede97d@hotmail.it>
2023-10-19 22:09:59 +0200

    redefintion of lucene interface

--- my-lucene-library/src/main/java/io/github/fededa/LuceneInterface.java
+++
@@ -1,9 +0,0 @@
-package io.github.fededa;
-
-public class LuceneInterface {
-
-}
-
-// Devo andare avanti, l'idea è quella di disegnare una libreria (far riferimento a chatgpt)
-// ogni homework ha il proprio progetto
-// fare dei test per vedere se la libreria si integra con progetti esistenti
---
+++ my-lucene-library/src/main/java/io/github/fededa/lucenecustomhandler/LuceneCustomHandler.java
@@ -0,0 +1,2 @@
+package io.github.fededa.lucenecustomhandler;public class LuceneCustomHandler {
+}
