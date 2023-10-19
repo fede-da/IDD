0180a43cbd3ccf9881e081f40bf31b1edca468c6
kaguyasama <fede97d@hotmail.it>
2023-10-19 22:30:58 +0200

    before crazy movements

--- my-lucene-library/src/main/java/io/github/fededa/lucenecustomhandler/LuceneCustomHandlerInterface.java
+++ my-lucene-library/src/main/java/io/github/fededa/lucenecustomhandler/LuceneCustomHandlerInterface.java
@@ -1,2 +1,18 @@
-package io.github.fededa.lucenecustomhandler;public interface LuceneCustomHandlerInterface {
+package io.github.fededa.lucenecustomhandler;
+
+/**
+ * Interface that defines interactions with Lucene
+ *
+ * @author Federico D'Armini
+ * @version 1.0
+ * @since 2023-10-20
+ */
+
+public interface LuceneCustomHandlerInterface {
+
+    /**
+     *  Runs 2nd Homework's assignments
+     */
+
+    void runHw_2();
 }
