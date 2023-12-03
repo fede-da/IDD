d61ed84f54d712e3ee561f0ea24c9d639e505c6d
kaguyasama <fede97d@hotmail.it>
2023-12-03 21:31:33 +0100

    Added some XPath queries and updated Article's "from_dict" method

--- hw4/src/utils/data_extractor.py
+++ hw4/src/utils/data_extractor.py
@@ -1,4 +1,3 @@
-
 from lxml import etree
 
 
@@ -7,6 +6,61 @@ class DataExtractor:
 
     def __init__(self, input_string: str):
         self.file_red = input_string
+
+    def _map_nodes_as_strings_list(self, nodes):
+        return list(map(lambda node: str(etree.tostring(node, pretty_print=True, method="xml").decode()).strip().replace(' xmlns:xlink="http://www.w3.org/1999/xlink"', '').strip(), nodes))
+
+    def _map_node_as_string(self, node):
+        return str(etree.tostring(node, pretty_print=True, method="xml").decode()).strip().replace(' xmlns:xlink="http://www.w3.org/1999/xlink"', '')
+
+    # must return a list of dictionaries
+    def _get_table_paragraphs(self, root):
+        paragraph_reference_nodes = root.xpath('//p[xref[@ref-type="table"]]')
+        paragraph_list = []
+
+        for paragraph in paragraph_reference_nodes:
+            para_to_add = {}
+            full_text = [paragraph.text or ""]
+
+            # Including text from immediate children (ignoring nested tags)
+            for child in paragraph:
+                if child.tail:
+                    full_text.append(child.tail)
+
+            para_to_add["text"] = ''.join(full_text).strip()
+
+            # Find all <xref> elements with an 'rid' attribute within the paragraph
+            xrefs = paragraph.xpath('.//xref[@rid]')
+            refs = []
+
+            # Extract corresponding <ref> elements
+            for xref in xrefs:
+                rid = xref.get('rid')
+                ref = root.xpath(f'//ref[@id="{rid}"]')
+                if ref:
+                    ref_content = self._map_node_as_string(ref[0])
+                    refs.append(ref_content)
+
+            # Add refs to the paragraph dictionary if any are found
+            if refs:
+                para_to_add["citation"] = refs
+
+            paragraph_list.append(para_to_add)
+
+        return paragraph_list
+
+    def _get_cell_contents(self, root):
+        # returns list of numbers as string
+        colspan_content_as_strings = root.xpath('//thead/tr/td/@colspan')
+        cells = []
+        for string in colspan_content_as_strings:
+            paragraphs_containing_string = root.xpath(f"//p[contains(text(), '{string}')]")
+            cells.append({
+                "content": string,
+                "cited_in": self._map_nodes_as_strings_list(paragraphs_containing_string)
+            })
+        return cells
+
 
     def extract_data(self) -> dict:
         # XPath
@@ -17,57 +71,47 @@ class DataExtractor:
         # This is a list!
         keywords_nodes = root.xpath('//kwd-group/kwd')
         table_wrap_id = root.xpath('//table-wrap/@id')[0]
+        caption_node = root.xpath('//caption/p')[0]
+        foots_node = root.xpath('//table-wrap-foot/p')
+        # complete below
+        table_node = root.xpath('//thead | //tbody')
+        paragraphs = self._get_table_paragraphs(root)
+        colspan_nodes = self._get_cell_contents(root)
         return {
             "pmcid": pmc_id_node.text,
-            "title": title_node.text,
-            # tostring method adds a string, trimmed with "replace" function
-            "abstract": str(etree.tostring(abstract_node, pretty_print=True, method="xml").decode()).replace(' xmlns:xlink="http://www.w3.org/1999/xlink"', ''),
-            "keywords": list(map(lambda node: node.text, keywords_nodes)),
-            "tables": [
-                {
-                    "table_id": table_wrap_id,
-                    "body": "",
-                    "caption": "",
-                    "caption_citations": [],
-                    # foots is a list of texts
-                    "foots": [],
-                    "paragraph":
-                    [
-                        {
-                            # here goes the text of the paragraph where the table was cited
-                            "text": "",
-                            # bibliography refence if a paper was cited in extracted text
-                            "citations": []
-                        }
-                    ],
-                    "cells":
-                    [
-                        {
-                            # text contained in a cell
-                            "content": "",
-                            # text of the paragraph where the text of the cell was mentioned –if the content is repeated in more than one cell, still use one element to report them
-                            "cited_in": []
-                        }
-                    ]
-
-                 }
-            ],
-            "figures":
-                [
+            "content": {
+                "title": title_node.text,
+                # tostring method adds a string, trimmed with "replace" function
+                "abstract": self._map_node_as_string(abstract_node),
+                "keywords": self._map_nodes_as_strings_list(keywords_nodes),
+                "tables": [
                     {
-                        "fig_id": "",
-                        "src": "",
-                        "caption": "",
-                        "paragraph":
-                        [
-                            {
-                                "cited_in": [],
-                                "citations": []
-                            }
-                        ]
+                        "table_id": table_wrap_id,
+                        "body": self._map_nodes_as_strings_list(table_node),
+                        "caption": self._map_node_as_string(caption_node),
+                        "caption_citations": [],
+                        # foots is a list of texts
+                        "foots": self._map_nodes_as_strings_list(foots_node),
+                        "paragraph": paragraphs,
+                        "cells":colspan_nodes
+                     }
+                ],
+                "figures":
+                    [
+                        {
+                            "fig_id": "",
+                            "src": "",
+                            "caption": "",
+                            "paragraph":
+                            [
+                                {
+                                    "cited_in": [],
+                                    "citations": []
+                                }
+                            ]
 
-                    },
-                ]
+                        },
+                    ]
+            }
         }
-
 
