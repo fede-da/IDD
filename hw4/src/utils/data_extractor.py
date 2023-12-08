8f8fbb65aa1f1dc2b6ec8672099bf040a44b9e9d
kaguyasama <fede97d@hotmail.it>
2023-12-08 13:09:33 +0100

    Some bugs fixed, project completed

--- hw4/src/utils/data_extractor.py
+++ hw4/src/utils/data_extractor.py
@@ -51,15 +51,17 @@ class DataExtractor:
         return paragraph_list
 
     def _get_cell_contents(self, node):
-        # returns list of numbers as string
         td_content = node.xpath('.//td/text()')
+        all_paragraphs = node.xpath('.//p')
         cells = []
+
         for string in td_content:
-            paragraphs_containing_string = node.xpath(f".//p[contains(text(), \"{string}\")]")
+            paragraphs_containing_string = [p for p in all_paragraphs if p.text and string in p.text]
             cells.append({
                 "content": string,
                 "cited_in": self._map_nodes_as_strings_list(paragraphs_containing_string)
             })
+
         return cells
 
     def _extract_figures(self, root, pmc_id):
@@ -78,8 +80,12 @@ class DataExtractor:
                 if ref:
                     ref_content = self._map_node_as_string(ref[0])
                     caption_citations.append(ref_content)
-            source = fig.xpath('.//graphic/@xlink:href', namespaces=namespaces)[0]
-            source = f"https://www.ncbi.nlm.nih.gov/pmc/articles/{pmc_id}/bin/{source}.jpg"
+            source_node = fig.xpath('.//graphic/@xlink:href', namespaces=namespaces)
+            source = source_node[0] if source_node else None
+            if source:
+                source = f"https://www.ncbi.nlm.nih.gov/pmc/articles/{pmc_id}/bin/{source}.jpg"
+            else:
+                source = ""
             # TODO: Completare estrazione citations slide 14
             paragraph = {}
             cited_in_paragraph_nodes = root.xpath(f"//p[xref[@ref-type='fig' and @rid='{fig_id}']]")
@@ -94,7 +100,7 @@ class DataExtractor:
                             citations_paragraphs.append(ref_content)
             paragraph["cited_in"] = self._map_nodes_as_strings_list(cited_in_paragraph_nodes)
             paragraph["citations"] = citations_paragraphs
-            figures.append({"id": fig_id, "src": source, "caption": caption_paragraph.strip(), "caption_citations": caption_citations, "paragraph": paragraph})
+            figures.append({"id": fig_id, "src": source, "caption": caption_paragraph, "caption_citations": caption_citations, "paragraph": paragraph})
         return figures
 
 
@@ -104,7 +110,7 @@ class DataExtractor:
         tables_wrap = root.xpath('//table-wrap')
         for table in tables_wrap:
             table_id = table.get('id')
-            caption_node = table.xpath('.//caption/p')[0]
+            caption_node = table.xpath('.//caption/p')[0] if table.xpath('.//caption/p') else None
             foots_node = table.xpath('.//table-wrap-foot/p')
             # complete below
             table_node = table.xpath('.//thead | .//tbody')
@@ -119,7 +125,7 @@ class DataExtractor:
             ret.append({
                     "table_id": table_id,
                     "body": self._map_nodes_as_strings_list(table_node),
-                    "caption": self._map_node_as_string(caption_node),
+                    "caption": self._map_node_as_string(caption_node) if caption_node else None,
                     "caption_citations": caption_citations,
                     "foots": self._map_nodes_as_strings_list(foots_node),
                     "paragraphs": paragraphs,
@@ -130,20 +136,21 @@ class DataExtractor:
     def extract_data(self) -> dict:
         # XPath
         root = etree.fromstring(self.file_red)
-        pmc_id_node = root.xpath('//article-id[@pub-id-type="pmc"]')[0]
+        pmc_id_nodes = root.xpath('//article-id[@pub-id-type="pmc"]')
         title_node = root.xpath('//article-title')[0]
-        abstract_node = root.xpath('//abstract/*/p')[0]
+        abstract_nodes = root.xpath('//abstract/*/p')
+        abstract_node = abstract_nodes[0] if abstract_nodes else None
         # This is a list!
         keywords_nodes = root.xpath('//kwd-group/kwd')
         return {
-            "pmcid": pmc_id_node.text,
+            "pmcid": pmc_id_nodes[0].text if len(pmc_id_nodes) > 0 else "",
             "content": {
                 "title": title_node.text,
                 # tostring method adds a string, trimmed with "replace" function
-                "abstract": self._map_node_as_string(abstract_node),
+                "abstract": self._map_node_as_string(abstract_node) if abstract_nodes else "",
                 "keywords": list(map(lambda node: node.text, keywords_nodes)),
                 "tables": self._get_tables(root),
-                "figures": self._extract_figures(root, pmc_id_node.text)
+                "figures": self._extract_figures(root, pmc_id_nodes[0].text) if len(pmc_id_nodes) > 0 else []
 
             }
         }
