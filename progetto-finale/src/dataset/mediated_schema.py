0f4473dd8d259d04fab90a529ea0bb94f7cac453
kaguyasama <fede97d@hotmail.it>
2025-01-06 20:01:32 +0100

    mediated schema updated

--- progetto-finale/src/dataset/mediated_schema.py
+++ progetto-finale/src/dataset/mediated_schema.py
@@ -1,10 +1,5 @@
-schema = (
+schema = [
     "CompanyName",
     "MarketCap",
-    "Revenue",
-    "NetIncome",
-    "TotalAssets",
-    "TotalLiabilities",
-    "TotalEquity",
-    "SectorAndIndustry",  # Nota: Questo è un oggetto nidificato che include 'Sector' e 'Industry'
-)
+    "EmployeesTotalAmount"
+]
